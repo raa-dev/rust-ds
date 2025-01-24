64fad80f40b1bfe873062efd56ed505863dd3a9d
raa <rodrx20@gmail.com>
2025-01-23 23:26:19 -0600

    feat: gh-10 singly double list update method updated

--- src/linked_lists/double/mod.rs
+++ src/linked_lists/double/mod.rs
@@ -108,17 +108,38 @@ where
         }
         Err(Error::ValueNotFound)
     }
-    // fn update(&mut self, old_value: T, new_value: T) -> Result<bool, Error<T>> {
-    //     let mut current = &mut self.head;
-    //     while let Some(mut node) = current {
-    //         if *node.get_mut().get_value() == old_value {
-    //             node.get_mut().set_value(new_value);
-    //             return Ok(true);
-    //         }
-    //         current = &mut node.get_mut().get_next();
-    //     }
-    //     Err(Error::ValueNotFound { value: old_value })
-    // }
+
+    fn update(&mut self, old_value: T, new_value: T) -> Result<bool> {
+        match self.head.as_ref() {
+            None => return Err(Error::EmptyList),
+            Some(current) => {
+                let mut current = current;
+                if current.borrow().get_next().is_none() {
+                    return Ok(*current.borrow().get_value() == old_value);
+                }
+                while current.borrow().get_next().is_some() {
+                    if *current.borrow().get_value() == old_value {
+                        current.borrow_mut().set_value(new_value);
+                        return Ok(true);
+                    }
+                    current = unsafe {
+                        current
+                            .as_ptr()
+                            .as_ref()
+                            .unwrap()
+                            .get_next()
+                            .as_ref()
+                            .unwrap()
+                    };
+                }
+                if current.borrow().get_next().is_none() {
+                    current.borrow_mut().set_value(new_value);
+                    return Ok(true);
+                }
+            }
+        }
+        Err(Error::ValueNotFound)
+    }
 
     fn from_vec(values: Vec<T>) -> Self {
         let mut list = Self::new();
@@ -136,8 +157,8 @@ where
         match self.tail.as_ref() {
             None => Err(Error::EmptyList),
             Some(tail) => {
-                let mut tail = tail.clone();
-                let mut previous = tail
+                let tail = tail.clone();
+                let previous = tail
                     .borrow()
                     .get_previous()
                     .clone()
@@ -205,10 +226,12 @@ mod test {
         let value = list.pop().unwrap().unwrap();
         assert_eq!(value, 2);
         assert_eq!(list.len, 1);
-        let list2 = Double::from_vec(vec!["hello", "world", "rust"]);
+        let mut list2 = Double::from_vec(vec!["hello", "world", "rust"]);
         assert_eq!(list2.get(1).unwrap().unwrap(), "world");
         assert!(!list2.is_empty());
         assert!(list2.search("rust").unwrap());
+        assert!(list2.update("world", "earth").unwrap());
+        assert_eq!(list2.get(1).unwrap().unwrap(), "earth");
     }
 
     #[test]
